11041168e8212a6b2faa7b9906fcab057a1f72e9
Thiago Crepaldi <thiago.crepaldi@microsoft.com>
2020-06-17 18:00:05 -0700

    Initial implementation

--- orttraining/orttraining/test/python/orttraining_test_pytorch_trainer_frontend.py
+++ orttraining/orttraining/test/python/orttraining_test_pytorch_trainer_frontend.py
@@ -1,10 +1,12 @@
 import pytest
 import torch
+from numpy.testing import assert_allclose
 
 from onnxruntime.capi.training.optim import lr_scheduler
 from onnxruntime.capi.training.amp import loss_scaler
 from onnxruntime.capi.training import pytorch_trainer_options as pt_options
 from onnxruntime.capi.training import pytorch_trainer
+from onnxruntime.capi.training import optim
 
 
 @pytest.mark.parametrize("test_input", [
@@ -90,3 +92,17 @@ def testTrainStepInfoInvalidAllFinite(test_input):
 
     with pytest.raises(AssertionError):
         pytorch_trainer.TrainStepInfo(step=test_input)
+
+
+@pytest.mark.parametrize("optim_name", [
+    ('AdamOptimizer'),
+    ('LambOptimizer'),
+    ('SGDOptimizer')
+])
+def testOptimizerConfigs(optim_name):
+    '''Test initialization of _OptimizerConfig and its extensions'''
+    hyper_parameters={'lr':0.001}
+    cfg = optim.config._OptimizerConfig(name=optim_name, hyper_parameters=hyper_parameters, param_groups=[])
+    assert cfg.name == optim_name
+    rtol = 1e-03
+    assert_allclose(hyper_parameters['lr'], cfg.lr, rtol=rtol, err_msg="loss mismatch")
